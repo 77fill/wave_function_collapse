c71b8329ff2f6d2dd291038f9e105668781a29b1
Philipp Schmalz <77fill@googlemail.com>
2025-03-30 13:54:51 +0200

    fix(usecase): @Component for ShowTileImages

--- src/main/java/dev/pschmalz/wave_function_collapse/usecase/ShowTileImages.java
+++ src/main/java/dev/pschmalz/wave_function_collapse/usecase/ShowTileImages.java
@@ -5,7 +5,9 @@ import dev.pschmalz.wave_function_collapse.domain.collections_tuples.TileSet;
 import dev.pschmalz.wave_function_collapse.usecase.interfaces.View;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ShowTileImages {
     @Autowired
     private TileSet tiles;
