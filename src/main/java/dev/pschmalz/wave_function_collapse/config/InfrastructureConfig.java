4e3650e58b6385e12fb247a63c80cb5d74f97352
Philipp Schmalz <77fill@googlemail.com>
2025-04-05 13:01:23 +0200

    Add FileChooserImpl
    Add InfrastructureConfig bean def

--- src/main/java/dev/pschmalz/wave_function_collapse/config/InfrastructureConfig.java
+++ src/main/java/dev/pschmalz/wave_function_collapse/config/InfrastructureConfig.java
@@ -2,7 +2,9 @@ package dev.pschmalz.wave_function_collapse.config;
 
 import com.google.common.reflect.ClassPath;
 import dev.pschmalz.wave_function_collapse.infrastructure.classpath_store.ClasspathStoreImpl;
+import dev.pschmalz.wave_function_collapse.infrastructure.file_chooser.FileChooserImpl;
 import dev.pschmalz.wave_function_collapse.usecase.interfaces.ClasspathStore;
+import dev.pschmalz.wave_function_collapse.usecase.interfaces.FileChooser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -21,5 +23,10 @@ public class InfrastructureConfig {
     @Bean
     public ClasspathStore classpathStore() {
         return new ClasspathStoreImpl(allowedNameSuffixes, classPath, Path.of("dev", "pschmalz", "wave_function_collapse"));
+    }
+
+    @Bean
+    public FileChooser fileChooser() {
+        return new FileChooserImpl();
     }
 }
---
+++ src/main/java/dev/pschmalz/wave_function_collapse/infrastructure/file_chooser/FileChooserImpl.java
@@ -0,0 +1,32 @@
+package dev.pschmalz.wave_function_collapse.infrastructure.file_chooser;
+
+import dev.pschmalz.wave_function_collapse.usecase.interfaces.FileChooser;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.stream.Stream;
+
+public class FileChooserImpl implements FileChooser {
+    private final JFileChooser jFileChooser = new JFileChooser();
+
+    @Override
+    public Flux<File> chooseImagePaths(File startDirectory) {
+        jFileChooser.setCurrentDirectory(startDirectory);
+
+        return Flux.generate(sink -> {
+            var userAnswer = jFileChooser.showOpenDialog(null);
+            /*
+             * BLOCK TILL USER IS DONE<br>
+             * (But Flux is already returned)
+             */
+            if(userAnswer == JFileChooser.APPROVE_OPTION) {
+                Stream.of(jFileChooser.getSelectedFiles())
+                        .forEach(sink::next);
+            }
+
+            sink.complete();
+        });
+    }
+}
