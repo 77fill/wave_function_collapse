3ea09c00d00f2b2beceaa99d27a325236db79688
Philipp Schmalz <77fill@googlemail.com>
2025-04-05 21:39:38 +0200

    Implement MemoryGridStore (single file change)

--- src/main/java/dev/pschmalz/wave_function_collapse/domain/MemoryGridStore.java
+++ src/main/java/dev/pschmalz/wave_function_collapse/domain/MemoryGridStore.java
@@ -3,12 +3,36 @@ package dev.pschmalz.wave_function_collapse.domain;
 import dev.pschmalz.wave_function_collapse.domain.collections_tuples.TileSlotGrid;
 import reactor.core.publisher.Mono;
 
-public class MemoryGridStore {
-    public void set(TileSlotGrid grid) {
+import java.util.MissingResourceException;
+import java.util.Optional;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class MemoryGridStore {
+    private Optional<TileSlotGrid> tileSlotGrid;
+    private Lock tileSlotGridLock = new ReentrantLock();
+
+    public void set(TileSlotGrid grid) {
+        tileSlotGridLock.lock();
+        tileSlotGrid = Optional.of(grid);
+        tileSlotGridLock.unlock();
     }
 
     public Mono<TileSlotGrid> get() {
-        return null;
+        return Mono.<TileSlotGrid>create(
+                sink ->
+                   tileSlotGrid.ifPresentOrElse(
+                           sink::success,
+                           () -> sink.error(
+                                   new MissingResourceException(
+                                       "MemoryGridStone does not have a tileSlotGrid",
+                                       TileSlotGrid.class.getName(),
+                                       "tileSlotGrid"
+                                   )
+                           )
+                   )
+            )
+                .doOnSubscribe(s -> tileSlotGridLock.lock())
+                .doOnTerminate(tileSlotGridLock::unlock);
     }
 }
