31ef3723e5ca77df91e84b543b78dafd50a2dc6d
Philipp Schmalz <77fill@googlemail.com>
2025-04-13 15:08:54 +0200

    Refactor FileChooserImpl: Lombok & simplify

--- src/main/java/dev/pschmalz/wave_function_collapse/infrastructure/file_chooser/FileChooserImpl.java
+++ src/main/java/dev/pschmalz/wave_function_collapse/infrastructure/file_chooser/FileChooserImpl.java
@@ -4,37 +4,39 @@ import dev.pschmalz.wave_function_collapse.usecase.interfaces.FileChooser;
 import io.vavr.Function1;
 import io.vavr.collection.Stream;
 import io.vavr.control.Option;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 import javax.swing.*;
 import java.io.File;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class FileChooserImpl implements FileChooser {
-    private final JFileChooser jFileChooser;
-    private final Lock jFileChooserLock = new ReentrantLock();
+    JFileChooser jFileChooser;
+    Lock jFileChooserLock = new ReentrantLock();
 
     @Override
     public Stream<File> chooseImagePaths(File startDirectory) {
-        return Stream.continually(
-            () -> {
-                jFileChooserLock.lock();
-                // BLOCKS UNTIL FREE
 
-                jFileChooser.setCurrentDirectory(startDirectory);
-                var userAnswer = jFileChooser.showOpenDialog(null);
-                // BLOCKS UNTIL USER IS DONE
+        jFileChooserLock.lock();
+        // BLOCKS UNTIL FREE
 
-                var result = Option.when(
-                        userAnswer == JFileChooser.APPROVE_OPTION,
-                        jFileChooser::getSelectedFiles);
+        jFileChooser.setCurrentDirectory(startDirectory);
+        var userAnswer = jFileChooser.showOpenDialog(null);
+        // BLOCKS UNTIL USER IS DONE
 
-                jFileChooserLock.unlock();
-                return result;
-            }).take(1)
-                .flatMap(Function1.identity())
-                .flatMap(Stream::of);
+        var result = Option.when(
+                userAnswer == JFileChooser.APPROVE_OPTION,
+                jFileChooser::getSelectedFiles);
+
+        jFileChooserLock.unlock();
+
+        return Stream.of(result.getOrElse(new File[0]));
+
     }
 }
