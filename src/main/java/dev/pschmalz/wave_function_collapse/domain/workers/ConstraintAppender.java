4ce7c33644e3f4f2b3973d6f30976767ad6be5a3
Philipp Schmalz <77fill@googlemail.com>
2025-04-13 12:34:56 +0200

    Fix ConstraintAppender, Remove ConstraintCreator

--- src/main/java/dev/pschmalz/wave_function_collapse/domain/workers/ConstraintAppender.java
+++ src/main/java/dev/pschmalz/wave_function_collapse/domain/workers/ConstraintAppender.java
@@ -4,6 +4,7 @@ import dev.pschmalz.wave_function_collapse.domain.basic_elements.SmartConstraint
 import dev.pschmalz.wave_function_collapse.domain.basic_elements.Tile;
 import io.vavr.Function1;
 import io.vavr.Tuple2;
+import io.vavr.collection.Set;
 import io.vavr.collection.Stream;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -21,8 +22,8 @@ public class ConstraintAppender implements Function1<Stream<Tile>,Stream<Tile>> 
         return tiles.zip(Stream(tiles).cycle())
                     .map(to_tile_otherTiles)
                     .flatMap(toStream_tile_otherTile)
-                    .map(to_tile_constraint)
-                    .map(to_tileWithConstraint);
+                    .map(to_tile_constraints)
+                    .map(to_tileWithConstraints);
     }
 
     private Tuple2<Tile, Stream<Tile>> to_tile_otherTiles(Tile tile, Stream<Tile> allTiles) {
@@ -33,12 +34,12 @@ public class ConstraintAppender implements Function1<Stream<Tile>,Stream<Tile>> 
         return Stream(tile).cycle().zip(otherTiles);
     }
 
-    private Tuple2<Tile, SmartConstraint> to_tile_constraint(Tile tile, Tile otherTile) {
+    private Tuple2<Tile, Set<SmartConstraint>> to_tile_constraints(Tile tile, Tile otherTile) {
         return Tuple(tile, constraintGenerator.apply(tile,otherTile));
     }
 
-    private Tile to_tileWithConstraint(Tile tile, SmartConstraint constraint) {
-        return tile.withConstraints(Set(constraint));
+    private Tile to_tileWithConstraint(Tile tile, Set<SmartConstraint> constraints) {
+        return tile.withConstraints(constraints);
     }
 
     Function1<
@@ -49,9 +50,9 @@ public class ConstraintAppender implements Function1<Stream<Tile>,Stream<Tile>> 
             Stream<Tuple2<Tile,Tile>>> toStream_tile_otherTile = Function(this::toStream_tile_otherTile).tupled();
     Function1<
             Tuple2<Tile, Tile>,
-            Tuple2<Tile,SmartConstraint>> to_tile_constraint = Function(this::to_tile_constraint).tupled();
+            Tuple2<Tile,Set<SmartConstraint>>> to_tile_constraints = Function(this::to_tile_constraints).tupled();
     Function1<
-            Tuple2<Tile, SmartConstraint>,
-            Tile> to_tileWithConstraint = Function(this::to_tileWithConstraint).tupled();
+            Tuple2<Tile, Set<SmartConstraint>>,
+            Tile> to_tileWithConstraints = Function(this::to_tileWithConstraint).tupled();
 
 }
--- src/main/java/dev/pschmalz/wave_function_collapse/domain/workers/ConstraintCreator.java
+++
@@ -1,8 +0,0 @@
-package dev.pschmalz.wave_function_collapse.domain.workers;
-
-import dev.pschmalz.wave_function_collapse.domain.basic_elements.TileSlotGrid;
-
-import java.util.function.Consumer;
-
-public interface ConstraintCreator extends Consumer<TileSlotGrid> {
-}
